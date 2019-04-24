e61f87f7da6eb2381e226b59f995b99b08a6199f
Deepak Telkar <d66pak@users.noreply.github.com>
2019-04-25 08:52:51 +1000

    cisco openconnect client setup

---
+++ cisco_openconnect.sh
@@ -0,0 +1,7 @@
+sudo apt install network-manager-openconnect network-manager-openconnect-gnome
+
+# Only needed if you don't want to restart.
+#sudo systemctl restart network-manager.service
+
+# Openconnect from bash
+# sudo openconnect <url>
